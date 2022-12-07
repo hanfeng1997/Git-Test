844f4754dd489c8bd0f6ca4ab560393fe04a4e03
hanfeng <963583749@qq.com>
2022-12-07 16:31:00 +0800

    "commit3"

--- src/main/java/com/han/git/GitTest.java
+++ src/main/java/com/han/git/GitTest.java
@@ -3,9 +3,8 @@ package com.han.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, git!");
-        System.out.println("Hello, git!");
-        System.out.println("Hello, git!");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push new code");
     }
 }
