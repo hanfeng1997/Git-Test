8a10d500c988843dab9c65dfb3a4df024bc0b46d
hanfeng <963583749@qq.com>
2022-12-07 15:54:10 +0800

    "commit second"

--- src/main/java/com/han/git/GitTest.java
+++ src/main/java/com/han/git/GitTest.java
@@ -3,5 +3,7 @@ package com.han.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, git!");
+        System.out.println("Hello, git!");
+        System.out.println("Hello, git!");
     }
 }
